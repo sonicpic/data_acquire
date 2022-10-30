fa07a25578e10b50ed3d1504c254571ae5ad0584
smallgyro <20301114@bjtu.edu.cn>
2022-10-30 21:52:25 +0800

    pzh 10.30 完成了z轴加速度的采集

--- app/src/main/java/com/example/dataacquire/MainActivity.java
+++ app/src/main/java/com/example/dataacquire/MainActivity.java
@@ -23,12 +23,11 @@ import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Instant;
@@ -38,49 +37,56 @@ public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "sensor-sample";
     private TextView tv_accelerometer_sensor;
+    private TextView tv_magnetic_sensor;
+    private TextView tv_rotation_sensor;
+    private TextView tv_R;
+    private TextView tv_location;
+
+    private Button btn_file;
+    private Switch switch_start;
+
     private SensorManager mSensorManager;
     private MySensorEventListener mMySensorEventListener;
     private MyLocationListerner myLocationListerner;
 
-    // 定义LocationManager对象
-    private LocationManager locationManager;
-    private TextView tv_location;
-    private Button btn_start;
-    private Button btn_file;
+    private LocationManager locationManager;// 定义LocationManager对象
 
 
-    //加速度事件
-    SensorEvent accData=null;
-    //GPS数据
-    Location locationData=null;
+    float[] accValues = new float[3];//加速度数据
+    float[] magneticValues = new float[3];//地磁数据
+    Location locationData = null;//GPS数据
 
     //stream
     FileWriter fstream;
     BufferedWriter out;
 
+    //位置控制器种类
     private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         tv_accelerometer_sensor = findViewById(R.id.tv_accelerometer_sensor);
         tv_location = findViewById(R.id.location_tv);
-        btn_start = findViewById(R.id.btn_start);
+        tv_magnetic_sensor = findViewById(R.id.tv_magnetic_sensor);
+        tv_rotation_sensor = findViewById(R.id.tv_rotation_sensor);
+        tv_R = findViewById(R.id.tv_R);
         btn_file = findViewById(R.id.btn_file);
+        switch_start = findViewById(R.id.switch_start);
 
-        btn_start.setOnClickListener(new MyBtnListener());
-        btn_file.setOnClickListener(new MyBtnListener());
+        btn_file.setOnClickListener(new MyBtnListener());   //设置按钮监听器，打开文件管理
 
-        //打开文件流
+        /*打开文件流，并打印表头*/
         try {
-            fstream = new FileWriter(getExternalFilesDir("")+"/"+Instant.now().toString()+".csv");
+            fstream = new FileWriter(getExternalFilesDir("") + "/" + Instant.now().toString() + ".csv");
         } catch (IOException e) {
             e.printStackTrace();
         }
         out = new BufferedWriter(fstream);
         try {
-            out.write("acc_x,acc_y,acc_z,latitude,longitude,altitude,date,speed");
+            out.write("acc_x,acc_y,acc_z,latitude,longitude,altitude,date,speed");  //打印表头
             out.newLine();
         } catch (IOException e) {
             e.printStackTrace();
@@ -89,20 +95,16 @@ public class MainActivity extends AppCompatActivity {
         /*申请权限*/
         //  操作将用户引导至一个系统设置页面，在该页面上，用户可以为您的应用启用以下选项：授予所有文件的管理权限。
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R || Environment.isExternalStorageManager()) {
-
         } else {
             Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
             startActivity(intent);
         }
 
-
-
+        /*初始化*/
         this.mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE); //获取传感器服务
-
         this.locationManager = (LocationManager) getSystemService(LOCATION_SERVICE); //获取系统LocationManager服务
 
         this.mMySensorEventListener = new MySensorEventListener();
-
         this.myLocationListerner = new MyLocationListerner();
 
         //获取当前可用的位置控制器
@@ -134,6 +136,7 @@ public class MainActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+
         // 从GPS获取最近的定位信息
         Location location = locationManager.getLastKnownLocation(provider);
         // 将location里的位置信息显示在EditText中
@@ -141,6 +144,11 @@ public class MainActivity extends AppCompatActivity {
         // 设置每2秒获取一次GPS的定位信息
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                 500, 0, myLocationListerner);
+
+        //开启写数据线程
+        MyThread1 myThread1 = new MyThread1("start");
+        myThread1.start();
+
     }
 
 
@@ -150,13 +158,28 @@ public class MainActivity extends AppCompatActivity {
         if (mSensorManager == null) {
             return;
         }
-
+        /*注册加速度传感器监听*/
         Sensor accelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         if (accelerometerSensor != null) {
-            //注册传感器的监听器
             mSensorManager.registerListener(mMySensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_UI);
         } else {
             Log.d(TAG, "Accelerometer sensors are not supported on current devices.");
+        }
+
+        /*注册磁场传感器监听*/
+        Sensor magSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (magSensor != null) {
+            mSensorManager.registerListener(mMySensorEventListener, magSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Log.d(TAG, "magSensor sensors are not supported on current devices.");
+        }
+
+        /*注册旋转矢量传感器监听*/
+        Sensor rotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        if (rotationSensor != null) {
+            mSensorManager.registerListener(mMySensorEventListener, rotationSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Log.d(TAG, "rotationSensor sensors are not supported on current devices.");
         }
     }
 
@@ -166,19 +189,18 @@ public class MainActivity extends AppCompatActivity {
         if (mSensorManager == null) {
             return;
         }
-        //unregister all listener
-        mSensorManager.unregisterListener(mMySensorEventListener);
+        mSensorManager.unregisterListener(mMySensorEventListener);  //取消所有注册
     }
 
 
     @Override
     protected void onDestroy() {
-        // TODO Auto-generated method stub
         super.onDestroy();
-        //关闭时解除GPS监听器
+        /*解除GPS监听器*/
         if (locationManager != null) {
             locationManager.removeUpdates(myLocationListerner);
         }
+        /*关闭文件流*/
         try {
             out.close();
             fstream.close();
@@ -187,16 +209,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //传感器监听类
+    /*传感器监听类*/
     private class MySensorEventListener implements SensorEventListener {
         @Override
         public void onSensorChanged(SensorEvent event) {
+            /*加速度传感器数据*/
             if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
                 //Log.d(TAG, "accelerometer data[x:" + event.values[0] + ", y:" + event.values[1] + ", z:" + event.values[2] + "]");
                 tv_accelerometer_sensor.setText("[x:" + event.values[0] + ", y:" + event.values[1] + ", z:" + event.values[2] + "]");
-                accData = event;
-                //write();
+                accValues = event.values.clone();    //有没有可能有读写安全问题
             }
+            /*磁场传感器数据*/
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                //Log.d(TAG, "accelerometer data[x:" + event.values[0] + ", y:" + event.values[1] + ", z:" + event.values[2] + "]");
+                tv_magnetic_sensor.setText("[x:" + event.values[0] + ", y:" + event.values[1] + ", z:" + event.values[2] + "]");
+                magneticValues = event.values.clone();
+            }
+            /*旋转矢量传感器数据*/
+            if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                //Log.d(TAG, "accelerometer data[x:" + event.values[0] + ", y:" + event.values[1] + ", z:" + event.values[2] + "]");
+                tv_rotation_sensor.setText("[x:" + event.values[0] + ", y:" + event.values[1] + ", z:" + event.values[2] + "]");
+                //magneticValues = event.values.clone();
+            }
+            /*获取屏幕方向*/
+            float[] R = new float[9];
+            float[] values = new float[3];
+            mSensorManager.getRotationMatrix(R, null, accValues, magneticValues);
+            mSensorManager.getOrientation(R, values);
+            float gri = accValues[0] * R[6] + accValues[1] * R[7] + accValues[2] * R[8];
+            tv_R.setText(String.valueOf(gri));
+            //Log.d("MainActivity", "value[0] is " + Math.toDegrees(values[0]));
         }
 
         @Override
@@ -206,17 +248,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //GPS监听类
-    private class MyLocationListerner implements LocationListener{
+    private class MyLocationListerner implements LocationListener {
         @Override
         public void onLocationChanged(Location location) {
             // 当GPS定位信息发生改变时，更新位置
             locationData = location;
             updateView(location);
         }
+
         @Override
         public void onProviderDisabled(String provider) {
             updateView(null);
         }
+
         @SuppressLint("MissingPermission")
         @Override
         public void onProviderEnabled(String provider) {
@@ -224,6 +268,7 @@ public class MainActivity extends AppCompatActivity {
             updateView(locationManager
                     .getLastKnownLocation(provider));
         }
+
         @Override
         public void onStatusChanged(String provider, int status,
                                     Bundle extras) {
@@ -231,23 +276,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //线程子类
-    private class MyThread1 extends Thread{
+    private class MyThread1 extends Thread {
 
         private String name; //窗口名, 也即是线程的名字
 
-        public MyThread1(String name){
-            this.name=name;
+        public MyThread1(String name) {
+            this.name = name;
         }
 
         //循环打印数据
         @Override
-        public void run(){
-            while(true){
-                write();
-                try {
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        public void run() {
+            while (true) {
+                if (switch_start.isChecked()) {
+                    write(accValues, locationData);
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
@@ -257,10 +304,7 @@ public class MainActivity extends AppCompatActivity {
     private class MyBtnListener implements View.OnClickListener {
         @Override
         public void onClick(View arg0) {
-            if(arg0.getId()==R.id.btn_start){
-                MyThread1 myThread1 = new MyThread1("start");
-                myThread1.start();
-            }else if(arg0.getId()==R.id.btn_file){
+            if (arg0.getId() == R.id.btn_file) {
                 String path = "%2fAndroid%2fdara%2f";
                 Uri uri = Uri.parse("content://com.android.externalstorage.documents/document/primary:" + path);
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
@@ -297,43 +341,43 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    static{
+    static {
 
     }
+
     // 写入文件
-    private void write(){
-        //acc_x,acc_y,acc_z,latitude,longitude,altitude,date,speed
-        String acc_x,acc_y,acc_z;
-        if(accData != null){
-            acc_x = String.valueOf(accData.values[0]);
-            acc_y = String.valueOf(accData.values[1]);
-            acc_z = String.valueOf(accData.values[2]);
-        }else{
+    private void write(float[] accData, Location locationData) {
+        String acc_x, acc_y, acc_z;
+        if (accData != null) {
+            acc_x = String.valueOf(accData[0]);
+            acc_y = String.valueOf(accData[1]);
+            acc_z = String.valueOf(accData[2]);
+        } else {
             acc_x = "NULL";
             acc_y = "NULL";
             acc_z = "NULL";
         }
-        String latitude,longitude,altitude,speed;
-        if(locationData != null){
+        String latitude, longitude, altitude, speed;
+        if (locationData != null) {
             latitude = String.valueOf(locationData.getLatitude());
             longitude = String.valueOf(locationData.getLongitude());
             altitude = String.valueOf(locationData.getAltitude());
             speed = String.valueOf(locationData.getSpeed());
-        }else{
+        } else {
             latitude = "NULL";
             longitude = "NULL";
             altitude = "NULL";
             speed = "NULL";
         }
         try {
-            out.write(acc_x+","
-                    +acc_y+","
-                    +acc_z+","
-                    +latitude+","
-                    +longitude+","
-                    +altitude+","
-                    +Instant.now().toString()+","
-                    +speed);
+            out.write(acc_x + ","
+                    + acc_y + ","
+                    + acc_z + ","
+                    + latitude + ","
+                    + longitude + ","
+                    + altitude + ","
+                    + Instant.now().toString() + ","
+                    + speed);
             out.newLine();
             out.flush();
             //Log.d("PAN","out");
